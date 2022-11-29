c3791c20012e923e12f3062f30799053a36bfdc8
Yaroslav Fedunkiv <ya.fedunkiv@gmail.com>
2022-11-29 21:09:47 +0200

    lesson 33

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ import {Component, OnInit} from '@angular/core';
 export class ServersComponent implements OnInit{
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -23,5 +24,9 @@ export class ServersComponent implements OnInit{
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: any){
+  this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
